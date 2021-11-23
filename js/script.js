498332aaa040a8a68ed524fee303207ba1444a15
Nelia Lanets <78198715+nelialanets@users.noreply.github.com>
2021-11-23 11:55:32 -0800

    add  javascript

--- js/script.js
+++ js/script.js
@@ -12,8 +12,25 @@ getting data fro the server useing fetch
        + this.apiKey
        )
    .then((response) => response.json())
-   .then((data) => (data));
+   .then((data) => then.showWeaher(data));
    },
    /*-----------------------------
  DOM Vanilla JS
 --------------------------------------------------*/
+showWeather: function(data){
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const{ speed } = data.wind;
+    //console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = " Weather in "  + name;
+    document.querySelector(".icon").src =
+    "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity:" + humidity + "%";
+    document.querySelector(".wind").innerText= "Wind speed:" + speed + "km/h"
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" +  name + ")";
+    //console weather.getWeather("Ukraine")
+  
